12b91f82158864df3f739747c579d19aaa133208
gbrl1 <ghaywood5@gmail.com>
2021-06-25 13:59:33 -0400

    Created skeleton for the quiz test class
    Created page factory class for quiz form
    
    Initial commit

---
+++ WixSeleniumTest/src/website/QuizTest.java
@@ -0,0 +1,29 @@
+package website;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class QuizTest {
+	
+	@BeforeClass
+	public void setUp() {
+		
+	}
+	
+	@Test
+	public void form() {
+		
+	}
+	
+	@Test
+	public void buttonTest() {
+		
+	}
+	
+	@AfterClass
+	public void tearDown() {
+		
+	}
+
+}
