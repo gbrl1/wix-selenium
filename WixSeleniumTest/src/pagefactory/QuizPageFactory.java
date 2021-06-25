95e20dcf3ef9cb481d6e89a4daff6a33ad0cd00b
gbrl1 <ghaywood5@gmail.com>
2021-06-25 14:37:05 -0400

    Partially implemented QuizTest and QuizPageFactory and
    added a Test Suite file

--- WixSeleniumTest/src/pagefactory/QuizPageFactory.java
+++ WixSeleniumTest/src/pagefactory/QuizPageFactory.java
@@ -1,5 +1,42 @@
 package pagefactory;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class QuizPageFactory {
+	
+	WebDriver driver;
+	
+	@FindBy(id="input_comp-kq16038l1")
+	WebElement firstNameForm;
+	
+	@FindBy(id="input_comp-kq16038t1")
+	WebElement lastNameForm;
+	
+	@FindBy(id="comp-kq16ih16")
+	WebElement dateForm;
+	
+	@FindBy(xpath="//tbody/tr[1]/td[3]/button[1]/span[1]")
+	WebElement date;
+	
+	public QuizPageFactory (WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+	}
+	
+	public void setFirstName(String firstName) {
+		firstNameForm.sendKeys(firstName);
+	}
+	
+	public void setLateName(String lastName) {
+		lastNameForm.sendKeys(lastName);
+	}
+	
+	public void setDate() {
+		dateForm.click();
+		date.click();
+	}
 
 }
--- WixSeleniumTest/src/website/QuizTest.java
+++ WixSeleniumTest/src/website/QuizTest.java
@@ -1,19 +1,38 @@
 package website;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import pagefactory.QuizPageFactory;
+
 public class QuizTest {
+	
+	private WebDriver driver;
+	private String baseUrl;
+	QuizPageFactory quizPage;
 	
 	@BeforeClass
 	public void setUp() {
+		driver = new ChromeDriver();
+		baseUrl = "https://ghaywood5.wixsite.com/selenium/quiz-1";
 		
+		quizPage = new QuizPageFactory(driver);
+		
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get(baseUrl);
 	}
 	
 	@Test
 	public void form() {
-		
+		quizPage.setFirstName("John");
+		quizPage.setLateName("Doe");
+		quizPage.setDate();
 	}
 	
 	@Test
@@ -22,8 +41,9 @@ public class QuizTest {
 	}
 	
 	@AfterClass
-	public void tearDown() {
-		
+	public void tearDown() throws Exception {
+		Thread.sleep(2000);
+		driver.quit();
 	}
 
 }
